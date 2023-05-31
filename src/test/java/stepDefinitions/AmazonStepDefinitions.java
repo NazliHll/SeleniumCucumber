efb048a0a03e6e215021724d45714060e3e5cd49
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-31 12:35:36 +0300

    31.05.2023

---
+++ src/test/java/stepDefinitions/AmazonStepDefinitions.java
@@ -0,0 +1,22 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class AmazonStepDefinitions {
+    @Given("kullanici amazon anasayfasinda")
+    public void kullanici_amazon_anasayfasinda() {
+    }
+
+    @Then("kullanici Nutella icin arama yapar")
+    public void kullanici_nutella_icin_arama_yapar() {
+    }
+
+    @Then("sonuclarin Nutella icerdigini test eder")
+    public void sonuclarin_nutella_icerdigini_test_eder() {
+    }
+
+    @Then("sayfayi kapatir")
+    public void sayfayi_kapatir() {
+    }
+}
---
+++ src/test/resources/features/amazonSearch.feature
@@ -0,0 +1,8 @@
+Feature: Amazon Search
+
+  Scenario: TC01 kullanici amazonda kelime aratir
+
+    Given kullanici amazon anasayfasinda
+    Then kullanici Nutella icin arama yapar
+    And sonuclarin Nutella icerdigini test eder
+    And sayfayi kapatir
