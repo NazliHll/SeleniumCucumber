f745a5637a32fae3c44f8d9bf5b77fad3e19c254
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-06-01 16:21:28 +0300

    01.06.2023

--- src/test/java/stepDefinitions/HerokuappStepDefinition.java
+++ src/test/java/stepDefinitions/HerokuappStepDefinition.java
@@ -2,21 +2,28 @@ package stepDefinitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import pages.HerokuappPage;
 
 public class HerokuappStepDefinition {
+    HerokuappPage herokuappPage=new HerokuappPage();
     @And("add element butonuna basar")
     public void addElementButonunaBasar() {
+        herokuappPage.addElementButton.click();
     }
 
     @Then("delete butonu gorunur oluncaya kadar bekler")
     public void deleteButonuGorunurOluncayaKadarBekler() {
+
     }
 
     @And("delete butonunun gorunur oldugunu test eder")
     public void deleteButonununGorunurOldugunuTestEder() {
+        Assert.assertTrue(herokuappPage.deleteButton.isDisplayed());
     }
 
     @Then("delete butonuna basar")
     public void deleteButonunaBasar() {
+        herokuappPage.deleteButton.click();
     }
 }
