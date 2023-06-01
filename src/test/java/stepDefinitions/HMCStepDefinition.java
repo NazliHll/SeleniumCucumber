4dca945555a96c9664549f7639099904079581eb
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-06-01 11:42:17 +0300

    01.06.2023

---
+++ src/test/java/stepDefinitions/HMCStepDefinition.java
@@ -0,0 +1,47 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import pages.HMCPage;
+import utilities.ConfigReader;
+
+public class HMCStepDefinition {
+    HMCPage hmcPage=new HMCPage();
+
+    @Then("Log in  yazisina tiklar")
+    public void logInYazisinaTiklar() {
+        hmcPage.loginButonu.click();
+    }
+
+    @And("gecerli username girer")
+    public void gecerliUsernameGirer() {
+        hmcPage.usernameKutusu.sendKeys(ConfigReader.getProperty("hmcValidUsername"));
+    }
+
+    @And("gecerli password girer")
+    public void gecerliPasswordGirer() {
+        hmcPage.passwordKutusu.sendKeys(ConfigReader.getProperty("hmcValidPassword"));
+    }
+
+    @And("Login butonuna basar")
+    public void loginButonunaBasar() {
+        hmcPage.loginButonu2.click();
+    }
+
+    @Then("sayfaya giris yaptigini kontrol eder")
+    public void sayfayaGirisYaptiginiKontrolEder() {
+        Assert.assertTrue(hmcPage.hotelManagement.isDisplayed());
+    }
+
+    @And("kullanici gelismis butonuna basar")
+    public void kullaniciGelismisButonunaBasar() {
+        hmcPage.gelismis.click();
+    }
+
+    @Then("ilerle baglantisina tiklar")
+    public void ilerleBaglantisinaTiklar() {
+        hmcPage.ilerle.click();
+    }
+}
--- target/test-classes/features/US1004_ConfigDosyasindanParametreKullanimi.feature
+++ target/test-classes/features/US1004_ConfigDosyasindanParametreKullanimi.feature
@@ -1,7 +1,9 @@
 Feature: US1004 kullanici parametre ile configuration file'i kullanabilmeli
-
+@config
   Scenario: TC07 configuration properties dosyasindan parametre kullanimi
 
     Given kullanici "amazonUrl" anasayfasinda
+    Then kullanici 3 sn bekler
     And url'in "amazon" icerdigini test eder
     Then sayfayi kapatir
+
