5b379f61f48fbcd93a75947db8add2dd4a116eb6
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-06-01 16:16:53 +0300

    01.06.2023

---
+++ src/test/java/stepDefinitions/HerokuappStepDefinition.java
@@ -0,0 +1,22 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class HerokuappStepDefinition {
+    @And("add element butonuna basar")
+    public void addElementButonunaBasar() {
+    }
+
+    @Then("delete butonu gorunur oluncaya kadar bekler")
+    public void deleteButonuGorunurOluncayaKadarBekler() {
+    }
+
+    @And("delete butonunun gorunur oldugunu test eder")
+    public void deleteButonununGorunurOldugunuTestEder() {
+    }
+
+    @Then("delete butonuna basar")
+    public void deleteButonunaBasar() {
+    }
+}
---
+++ src/test/resources/features/US1010_Herokuapp.feature
@@ -0,0 +1,9 @@
+Feature: US1010 herokuapp delete testi
+
+  Scenario :TC15 herokuappten delete butonu calismali
+    Given kullanici "herokuappUrl" anasayfasinda
+    And add element butonuna basar
+    Then delete butonu gorunur oluncaya kadar bekler
+    And delete butonunun gorunur oldugunu test eder
+    Then delete butonuna basar
+    And sayfayi kapatir
