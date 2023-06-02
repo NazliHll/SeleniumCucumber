07e4714f5a243a6a5f91ef941987bfd69bfad7e2
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-06-02 17:30:11 +0300

    02.06.2023

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,7 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-reports.html"},//raporlama  için gerekli plugin
+        plugin = {"html:target/cucumber-reports.html",//raporlama  için gerekli plugin
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "stepDefinitions",
         tags = "@guru",
