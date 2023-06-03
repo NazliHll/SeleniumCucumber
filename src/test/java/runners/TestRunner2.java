c972677c4d01468d9d1fe67401d1b8ee8e590db8
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-06-03 10:49:48 +0300

    03.06.2023

---
+++ src/test/java/runners/TestRunner2.java
@@ -0,0 +1,18 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-reports.html",//raporlama  için gerekli plugin
+                "json:target/json-reports/cucumber2.json",
+                "junit:target/xml-report/cucumber2.xml"},
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
+        tags = "@parametre",
+        dryRun = false
+)
+public class TestRunner2 {
+}
